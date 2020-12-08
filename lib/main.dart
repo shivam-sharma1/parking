f8a14b62c739e48cfe5bd04ee79961ddb57e2328
Shivam <shivam0sharma85@gmail.com>
2020-12-08 16:26:36 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Add(),
+));
+class Add extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<Add> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back
+          ),
+          onPressed: (){},
+        ),
+        title: Text('Blacklist'),
+        backgroundColor: Colors.purple,
+        centerTitle: true,
+      ),
+      body:Container(
+        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Text(
+                "Add customers to blacklist",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              buildTextField("Full Name", "Enter Name"),
+              buildTextField("Brand", "Enter Vehicle brand"),
+              buildTextField("Vehicle Number", "Enter Vehicle number"),
+              buildTextInt("Mobile Number", "Enter mobile number"),
+              buildTextArea("Reason for Blacklisting", "Please specify the reason for blacklisting"),
+              SizedBox(
+                height: 35,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlineButton(
+                    padding: EdgeInsets.symmetric(horizontal: 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    onPressed: () {
+                      Navigator.push(context,
+                        MaterialPageRoute(builder:(context)=> Add()),
+                      );
+                    },
+                    child: Text("CANCEL",
+                        style: TextStyle(
+                            fontSize: 14,
+                            letterSpacing: 2.2,
+                            color: Colors.black)),
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return Dialog(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                            elevation: 16,
+                            child: Container(
+                              height: 400.0,
+                              width: 360.0,
+                                child: Center(
+                                  child: Text(
+                                    "Blacklist Successful",
+                                    style: TextStyle(fontSize: 24, color: Colors.green, fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    color: Colors.green,
+                    padding: EdgeInsets.symmetric(horizontal: 50),
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text(
+                      "SAVE",
+                      style: TextStyle(
+                          fontSize: 14,
+                          letterSpacing: 2.2,
+                          color: Colors.white),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField(
+      String labelText, String placeholder) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(bottom: 3),
+            labelText: labelText,
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            hintText: placeholder,
+            hintStyle: TextStyle(
+              fontSize: 16,
+              color: Colors.black,
+            )),
+      ),
+    );
+  }
+
+  Widget buildTextInt(
+      String labelText, String placeholder) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: TextField(
+          keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(bottom: 3),
+            labelText: labelText,
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            hintText: placeholder,
+
+            hintStyle: TextStyle(
+              fontSize: 16,
+              color: Colors.black,
+            )),
+      ),
+    );
+  }
+
+  Widget buildTextArea(
+      String labelText, String placeholder) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(bottom: 3),
+            labelText: labelText,
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            hintText: placeholder,
+            hintStyle: TextStyle(
+              fontSize: 16,
+              color: Colors.black,
+            )),
+        keyboardType: TextInputType.multiline,
+        maxLines: null,
+      ),
+    );
+  }
+}
